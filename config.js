0a4937f73f836a58426677d102a6b089e0dd7ddb
TheWale <58726425+TheWa1e@users.noreply.github.com>
2021-04-27 19:22:20 +0300

    update config.js from heroku server

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql2');
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : '07152000',
-	database : 'testing'
+	host     : 'us-cdbr-east-03.cleardb.com',
+	user     : 'b6f5b64d7d2d5f',
+	password : '4a242ef1',
+	database : 'heroku_e3915428adced65'
 });
 connection.connect(function(err){
 	if(!err){
